d189c188840298598d03b809962dcd5c0652f612
DakshWHT <93590885+DakshWHT@users.noreply.github.com>
2022-06-15 16:16:21 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+function addUser(){
+P1 = document.getElementById("player1_name_input").value;
+P2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", P1);
+    localStorage.setItem("player2_name", P2);
+}
